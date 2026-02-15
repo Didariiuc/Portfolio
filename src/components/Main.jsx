693a9cd5067c987bef72a0de38bd34ef0567b925
Your Name <you@example.com>
2026-02-15 21:48:18 +0600

    somthing tested

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
 import NavBar from "./NavBar"
 
+
+
 export default function main() {
   return (
     <div className="bg-indigo-600">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    screen:{
-        ssm: '375px',
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        
+    screens: {
+      ssm: '375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
     },
     extend: {},
   },
