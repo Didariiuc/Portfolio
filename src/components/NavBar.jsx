ea21c0b9e33eb11b853d0739582119a8f98f5474
Your Name <you@example.com>
2026-02-15 21:14:37 +0600

    tailwind config added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,23 +3,25 @@ import { CgNametag } from "react-icons/cg";
 
 function NavBar() {
   return (
-   <div>
-     <div>
-      <div className='flex items-center justify-between p-10 lg:flex-row'>
-        <div >
-          <a className='text-white font-mono text-3xl tracking-wider flex items-center' href="#"><CgNametag/>Havaa Tech</a>
+    <div>
+      <div>
+        <div className='flex items-center justify-between p-10 lg:flex-row'>
+          <div >
+            <a className='text-white font-mono text-3xl tracking-wider flex items-center' href="#"><CgNametag />Havaa Tech</a>
+          </div>
+
+          <div className='space-x-4'>
+            <div className='ssm:hidden lg:block space-x-2'>
+              <a className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl' href="#">Skills</a>
+              <a className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl' href="#">Projects</a>
+              <a className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl' href="#">Testimonial</a>
+              <a className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl' href="#">Education</a>
+            </div>
+          </div>
         </div>
 
-        <div className='space-x-4'>
-          <a className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl' href="#">Skills</a>
-          <a className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl' href="#">Projects</a>
-          <a className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl' href="#">Testimonial</a>
-          <a className='text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl' href="#">Educational</a>
-        </div>
       </div>
-
     </div>
-   </div>
   )
 }
 
